c5ebc25068c52e9b34a8fa8f9fff531c722221e8
„benearne“ <b.arnold@posteo.de>
2025-06-19 17:47:55 +0200

    add append function

---
+++ linkedlist.js
@@ -0,0 +1,79 @@
+class Node {
+	constructor(value, nextNode = null) {
+		this.value = value;
+		this.nextNode = nextNode;
+	}
+}
+
+class LinkedList {
+  constructor() {
+	this.head = null;
+  }
+
+  append(value) {
+	// Adds new node containing value to the end of the list
+	const newNode = new Node(value);
+
+	if (this.head === null) {
+		this.head = newNode;
+		return;
+	}
+
+	let current = this.head;
+	while (current.nextNode !== null) {
+		current = current.nextNode;
+	}
+
+	current.nextNode = newNode;
+  }
+
+  prepend(value) {
+	// Adds new node containg value to the start of the list
+  }
+
+  size() {
+	// returns the total number of nodes in the list
+  }
+
+  head() {
+	// Returns the first node in the list
+  }
+
+  tail() {
+	// Returns the last node in the list
+  }
+
+  pop() {
+	// removes the last element from the list
+  }
+
+  contains(value) {
+	// Returns true if the passed in value is in the list an otherwise returns false
+  }
+
+  find(value) {
+	// returns the index of the nove containing value, or null if not found
+  }
+
+  toString() {
+	// represents your LinkedList objects as strings, so you can print them out and preview them in the console. 
+	// The format should be: ( value ) -> ( value ) -> ( value ) -> null
+  }
+}
+
+const list = new LinkedList();
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+
+// Ausgabe aller Werte
+let current = list.head;
+while (current !== null) {
+	console.log(current.value);
+	current = current.nextNode;
+}
+
+
